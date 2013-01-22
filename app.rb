293e7eb33c416cf0a67b22f4faee50e61c8e4e7e
Floyd Pink <imbleedingme@googlemail.com>
2013-01-21 22:51:09 -0500

    changes to start redirecting technology.harimenon.com
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- app.rb
+++ app.rb
@@ -1,23 +1,56 @@
 require 'bundler/setup'
 require 'sinatra'
 
-ROOT = "http://ApproachE.com"
-#ROOT = "http://dnagir.github.com/dnagir"
+ROOT = "http://www.harimenon.com"
 
 # Redirect
-# http://blog.approache.com/2011/12/knockoutjs-validations-video.html
+# http://technology.harimenon.com/2012/12/lzwCompress.js.html
 # to
-# http://ApproachE.com/blog/knockoutjs-validations-video/
-get %r[/(\d{4,4})/(\d{1,2})/(.+).html]i do |year, month, slug|
-  redirect "#{ROOT}/blog/#{slug}/", 301
+# http://www.harimenon.com/blog/2012/12/06/lzwCompress.js/
+get '/2013/01/note-to-self-could-be-useful-someday.html' do
+  redirect "#{ROOT}/blog/2013/01/16/note-to-self-could-be-useful-someday/", 301
+end
+get '/2013/01/single-click-deployment.html' do
+  redirect "#{ROOT}/blog/2013/01/15/single-click-deployment/", 301
+end
+get '/2012/12/aop-for-logging-in-net.html' do
+  redirect "#{ROOT}/blog/2012/12/16/aop-for-logging-in-net/", 301
+end
+get '/2012/12/lzwCompress.js.html' do
+  redirect "#{ROOT}/blog/2012/12/06/lzwCompress.js/", 301
+end
+get '/2012/12/useful-git-commands.html' do
+  redirect "#{ROOT}/blog/2012/12/06/useful-git-commands/", 301
+end
+get '/2012/11/json-minification.html' do
+  redirect "#{ROOT}/blog/2012/11/09/json-minification/", 301
+end
+get '/2012/09/convert-all-jpgs-in-folder-to-pdfs-with.html' do
+  redirect "#{ROOT}/blog/2012/09/04/convert-all-jpgs-in-folder-to-pdfs-with/", 301
+end
+get '/2012/05/creating-dynamic-types-in-net.html' do
+  redirect "#{ROOT}/blog/2012/05/24/creating-dynamic-types-in-net/", 301
+end
+get '/2012/02/detect-jquery-presence.html' do
+  redirect "#{ROOT}/blog/2012/02/02/detect-jquery-presence/", 301
+end
+get '/2012/02/array-min-max-jquery-map.html' do
+  redirect "#{ROOT}/blog/2012/02/01/array-min-max-jquery-map/", 301
+end
+get '/2010/11/sort-dropdownselect-list-items-using.html' do
+  redirect "#{ROOT}/blog/2010/11/10/sort-dropdownselect-list-items-using/", 301
+end
+get '/2009/08/displaying-version-for-microsoft.html' do
+  redirect "#{ROOT}/blog/2009/08/26/displaying-version-for-microsoft/", 301
+end
+get '/2009/06/adding-in-new-window-link-on-sharepoint.html' do
+  redirect "#{ROOT}/blog/2009/06/23/adding-in-new-window-link-on-sharepoint/", 301
 end
 
-
-
 # Redirect
-# http://blog.approache.com/search/label/DevTools
+# http://technology.harimenon.com/search/label/ClickOnce Deployment
 # to
-# http://dnagir.github.com/dnagir/blog/categories/devtools/
+# http://www.harimenon.com/blog/categories/clickonce-deployment/
 
 # DevTool -> devtools
 # asp.net -> asp-net
@@ -28,28 +61,22 @@ get '/search/label/:label' do |label|
   redirect "#{ROOT}/blog/categories/#{category}/", 301
 end
 
-
-
-
-
-# http://blog.approache.com/2011_12_01_archive.html
+# http://technology.harimenon.com/2011_12_01_archive.html
 # =>
-# http://approache.com/blog/archives/
+# http://www.harimenon.com/blog/archives/
 get '/:year_:month_:day_archive.html' do
   redirect "#{ROOT}/blog/archives/", 301
 end
 
 
-
-
-# blog.ApproachE.com -> ApproachE.com/blog
+# technology.harimenon.com -> www.harimenon.com/blog/categories/technical/
 get '/' do
-  redirect "#{ROOT}/blog/", 301
+  redirect "#{ROOT}/blog/categories/technical/", 301
 end
 
 
 # Feeds
-# blog.approache.com/feeds/posts/default => FeedBurner
+# technology.harimenon.com/feeds/posts/default => FeedBurner
 get '/feeds/posts/default' do
-  redirect "http://feeds.feedburner.com/DmitriyNagirnyak", 301
+  redirect "#{ROOT}/atom.xml", 301
 end
